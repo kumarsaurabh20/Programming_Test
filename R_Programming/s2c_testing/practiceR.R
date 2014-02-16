7c89224eab52a59b165f2d60081281943ce4daa4
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-02-16 21:58:06 +0100

    update

--- R_Programming/s2c_testing/practiceR.R
+++ R_Programming/s2c_testing/practiceR.R
@@ -1,4 +1,55 @@
 #========================================================
+Description
+
+Retrieve or set the row or column names of a matrix-like object.
+
+Usage
+
+rownames(x, do.NULL = TRUE, prefix = "row")
+rownames(x) <- value
+
+colnames(x, do.NULL = TRUE, prefix = "col")
+colnames(x) <- value
+Arguments
+
+x	
+a matrix-like R object, with at least two dimensions for colnames.
+
+do.NULL	
+logical. If FALSE and names are NULL, names are created.
+
+prefix	
+for created names.
+
+value	
+a valid value for that component of dimnames(x). For a matrix or array this is either NULL or a character vector of non-zero length equal to the appropriate dimension.
+
+See Also
+
+dimnames, case.names, variable.names.
+
+Examples
+
+m0 <- matrix(NA, 4, 0)
+rownames(m0)
+
+m2 <- cbind(1, 1:4)
+colnames(m2, do.NULL = FALSE)
+colnames(m2) <- c("x","Y")
+rownames(m2) <- rownames(m2, do.NULL = FALSE, prefix = "Obs.")
+m2
+
+#========================================================
+#to convert NA values to 1 from a matrix
+y <- which(is.na(test)==TRUE)
+> test[y] <- 1
+convertNa <- function(x) {
+y <- which(is.na(x)==TRUE)
+x[y] <- 1
+return(x)
+#or can also use replace(results.filter, is.na(results.filter), 1).. working equally well.
+}
+#========================================================
 Description paste()
 
 Concatenate vectors after converting to character.
@@ -244,6 +295,203 @@ Concatenate characters to the array elements:
  [7,] "12 a"  "12 b" 
  [8,] "43 a"  "43 b" 
 #========================================================
+As a language for statistical analysis, R has a comprehensive library of functions for generating random numbers from various statistical distributions.  In this post, I want to focus on the simplest of questions: How do I generate a random number?
+
+The answer depends on what kind of random number you want to generate.  Let's illustrate by example.
+
+Generate a random number between 5.0 and 7.5
+If you want to generate a decimal number where any value (including fractional values) between the stated minimum and maximum is equally likely, use the runif function. This function generates values from the Uniform distribution. Here's how to generate one random number between 5.0 and 7.5:
+
+
+> x1 <- runif(1, 5.0, 7.5)
+> x1
+[1] 6.715697
+Of course, when you run this, you'll get a different number, but it will definitely be between 5.0 and 7.5. You won't get the values 5.0 or 7.5 exactly, either.
+
+If you want to generate multiple random values, don't use a loop. You can generate several values at once by specifying the number of values you want as the first argument to runif. Here's how to generate 10 values between 5.0 and 7.5:
+
+
+> x2 <- runif(10, 5.0, 7.5)
+> x2
+ [1] 6.339188 5.311788 7.099009 5.746380 6.720383 7.433535 7.159988
+ [8] 5.047628 7.011670 7.030854
+Generate a random integer between 1 and 10
+This looks like the same exercise as the last one, but now we only want whole numbers, not fractional values. For that, we use the sample function:
+
+
+> x3 <- sample(1:10, 1)
+> x3
+[1] 4
+The first argument is a vector of valid numbers to generate (here, the numbers 1 to 10), and the second argument indicates one number should be returned. If we want to generate more than one random number, we have to add an additional argument to indicate that repeats are allowed:
+
+
+> x4 <- sample(1:10, 5, replace=T)
+> x4
+[1] 6 9 7 6 5
+Note the number 6 appears twice in the 5 numbers generated. (Here's a fun exercise: what is the probability of running this command and having no repeats in the 5 numbers generated?)
+
+Select 6 random numbers between 1 and 40, without replacement
+If you wanted to simulate the lotto game common to many countries, where you randomly select 6 balls from 40 (each labelled with a number from 1 to 40), you'd again use the sample function, but this time without replacement:
+
+
+> x5 <- sample(1:40, 6, replace=F)
+> x5
+[1] 10 21 29 12  7 31
+You'll get a different 6 numbers when you run this, but they'll all be between 1 and 40 (inclusive), and no number will repeat. Also, you don't actually need to include the replace=F option -- sampling without replacement is the default -- but it doesn't hurt to include it for clarity.
+
+Select 10 items from a list of 50
+You can use this same idea to generate a random subset of any vector, even one that doesn't contain numbers. For example, to select 10 distinct states of the US at random:
+
+
+> sample(state.name, 10)
+ [1] "Virginia"     "Oklahoma"     "Maryland"     "Michigan"    
+ [5] "Alaska"       "South Dakota" "Minnesota"    "Idaho"       
+ [9] "Indiana"      "Connecticut" 
+You can't sample more values than you have without allowing replacements:
+
+
+> sample(state.name, 52)
+Error in sample(state.name, 52) : 
+  cannot take a sample larger than the population when 'replace = FALSE'
+... but sampling exactly the number you do have is a great way to randomize the order of a vector. Here are the 50 states of the US, in random order:
+
+
+> sample(state.name, 50)
+ [1] "California"     "Iowa"           "Hawaii"        
+ [4] "Montana"        "South Dakota"   "North Dakota"  
+ [7] "Louisiana"      "Maine"          "Maryland"      
+[10] "New Hampshire"  "Rhode Island"   "Texas"         
+[13] "Florida"        "North Carolina" "Minnesota"     
+[16] "Arkansas"       "Pennsylvania"   "Colorado"      
+[19] "Idaho"          "Connecticut"    "Utah"          
+[22] "South Carolina" "Illinois"       "Ohio"          
+[25] "New Jersey"     "Indiana"        "Wisconsin"     
+[28] "Mississippi"    "Michigan"       "Wyoming"       
+[31] "West Virginia"  "Alaska"         "Georgia"       
+[34] "Vermont"        "Virginia"       "Oklahoma"      
+[37] "Washington"     "New Mexico"     "New York"      
+[40] "Delaware"       "Nevada"         "Alabama"       
+[43] "Kentucky"       "Missouri"       "Oregon"        
+[46] "Tennessee"      "Arizona"        "Massachusetts" 
+[49] "Kansas"         "Nebraska" 
+You could also have just used sample(state.name) for the same result -- sampling as many values as provided is the default.
+#========================================================
+Description
+
+duplicated() determines which elements of a vector or data frame are duplicates of elements with smaller subscripts, and returns a logical vector indicating which elements (rows) are duplicates.
+
+anyDuplicated(.) is a “generalized” more efficient shortcut for any(duplicated(.)).
+
+Usage
+
+duplicated(x, incomparables = FALSE, ...)
+
+## Default S3 method:
+duplicated(x, incomparables = FALSE,
+           fromLast = FALSE, nmax = NA, ...)
+
+## S3 method for class 'array'
+duplicated(x, incomparables = FALSE, MARGIN = 1,
+           fromLast = FALSE, ...)
+
+anyDuplicated(x, incomparables = FALSE, ...)
+## Default S3 method:
+anyDuplicated(x, incomparables = FALSE,
+           fromLast = FALSE, ...)
+## S3 method for class 'array'
+anyDuplicated(x, incomparables = FALSE,
+           MARGIN = 1, fromLast = FALSE, ...)
+Arguments
+
+x	
+a vector or a data frame or an array or NULL.
+
+incomparables	
+a vector of values that cannot be compared. FALSE is a special value, meaning that all values can be compared, and may be the only value accepted for methods other than the default. It will be coerced internally to the same type as x.
+
+fromLast	
+logical indicating if duplication should be considered from the reverse side, i.e., the last (or rightmost) of identical elements would correspond to duplicated = FALSE.
+
+nmax	
+the maximum number of unique items expected (greater than one).
+
+...	
+arguments for particular methods.
+
+MARGIN	
+the array margin to be held fixed: see apply, and note that MARGIN = 0 maybe useful.
+Examples
+
+x <- c(9:20, 1:5, 3:7, 0:8)
+## extract unique elements
+(xu <- x[!duplicated(x)])
+## similar, same elements but different order:
+(xu2 <- x[!duplicated(x, fromLast = TRUE)])
+
+## xu == unique(x) but unique(x) is more efficient
+stopifnot(identical(xu,  unique(x)),
+          identical(xu2, unique(x, fromLast = TRUE)))
+
+duplicated(iris)[140:143]
+
+duplicated(iris3, MARGIN = c(1, 3))
+anyDuplicated(iris) ## 143
+
+anyDuplicated(x)
+anyDuplicated(x, fromLast = TRUE)
+
+#========================================================
+Description
+
+unique returns a vector, data frame or array like x but with duplicate elements/rows removed.
+
+Usage
+
+unique(x, incomparables = FALSE, ...)
+
+## Default S3 method:
+unique(x, incomparables = FALSE, fromLast = FALSE,
+        nmax = NA, ...)
+
+## S3 method for class 'matrix'
+unique(x, incomparables = FALSE, MARGIN = 1,
+       fromLast = FALSE, ...)
+
+## S3 method for class 'array'
+unique(x, incomparables = FALSE, MARGIN = 1,
+       fromLast = FALSE, ...)
+Arguments
+
+x	
+a vector or a data frame or an array or NULL.
+
+incomparables	
+a vector of values that cannot be compared. FALSE is a special value, meaning that all values can be compared, and may be the only value accepted for methods other than the default. It will be coerced internally to the same type as x.
+
+fromLast	
+logical indicating if duplication should be considered from the last, i.e., the last (or rightmost) of identical elements will be kept. This only matters for names or dimnames.
+
+nmax	
+the maximum number of unique items expected (greater than one). See duplicated.
+
+...	
+arguments for particular methods.
+
+MARGIN	
+the array margin to be held fixed: a single integer.
+
+Examples
+
+x <- c(3:5, 11:8, 8 + 0:5)
+(ux <- unique(x))
+(u2 <- unique(x, fromLast = TRUE)) # different order
+stopifnot(identical(sort(ux), sort(u2)))
+
+length(unique(sample(100, 100, replace = TRUE)))
+## approximately 100(1 - 1/e) = 63.21
+
+unique(iris)
+#========================================================
 > tab
        a   b    c    d
 val1 210 201 2001 2020
@@ -290,12 +538,95 @@ result
 #finally return a data.frame with probe names and cell counts
 
 
-calCellCount <- function(probe, columns, cells, probeList, intValues) {
+calCellCount <- function(resultsFitCoeffs, inputNormValues) {
 
-     predictValues <- cbind(probeList, intValues)
-     matrixModels <- getFitCoefs(probe, columns, cells)
+results
+     probe.list                                                                           
+[1,] "POSITIVE_25_dT"          "1976.59485832975"  NA                  "27652.0979233343" 
+[2,] "Poly-T-CY5"              "23216.4176367556"  "-3006.41103953601" NA                 
+[3,] "EukS_1209_25_dT"         "-178.012909854657" "7310.55172892956"  "-10963.5995131028"
+[4,] "Test15 (EukS_1209_25dT)" "-1080.78528492518" "14047.5822563424"  "-28426.5913573295"
+[5,] "EukS_328_25_dT"          "-274.099682993555" "6707.72161256281"  "-12045.7096648079"
+
+
+results2
+  [,1]                      [,2]                 [,3]                 
+[1,] "POSITIVE_25_dT"          "1"                  "0.167405764966741"  
+[2,] "Poly-T-CY5"              "5.97350993377483"   "1"                  
+[3,] "EukS_1209_25_dT"         "0.0543415750053807" "0.00909709293327326"
+[4,] "Test15 (EukS_1209_25dT)" "0.0429492802417219" "0.00718995711363636"
+[5,] "EukS_328_25_dT"          "0.0604613039031457" "0.0101215708307927" 
+
+
+
+
+t(sapply(intersect(rownames(tab), rownames(tab2)), 
+      function(x) outer(as.matrix(tab[x, ]), as.matrix(tab2[x, ])))
+    )
+
+#to convert NA values to 1 from a matrix
+y <- which(is.na(test)==TRUE)
+> test[y] <- 1
+convertNa <- function(x) {
+y <- which(is.na(x)==TRUE)
+x[y] <- 1
+return(x)
+#or can also use replace(results.filter, is.na(results.filter), 1).. working equally well.
+}
+
+#===========================================================================================================
+getPrediction <- function(results, results2) {
+     
+match.probes <- match(results2[,1], results[,1])
+results.filter <- results[, -1]
+results2.filter <- results2[, -1]
+results2.filter <- apply(results2.filter,2, function(x) as.numeric(x))
+results.filter <- apply(results.filter,2, function(x) as.numeric(x))
+results.filter <- convertNa(results.filter)
+dummyMatrix <- matrix(1, nrow(results2.filter), 1)
+results2.filter <- cbind(dummyMatrix, results2.filter)
+new.metrix <- results.filter*results2.filter
+cell.counts <- vector()
+for (i in c(1:nrow(results2.filter))) { cell.counts[i] <- sum(new.metrix[i,])}
+
+return(cell.counts)
+}
+
 
 }
+
+
+selectProbeFromList <- function(results, results2) {
+
+commonProbesInTwoResults <- intersect(results[,1], results2[,1])
+
+selectedProbesFromGpr <- matrix(0, length(commonProbesInTwoResults), ncol(results2))
+  
+  for (i in c(1:length(commonProbesInTwoResults))) {
+  selectedProbesFromGpr[i,] <- subset(results2, commonProbesInTwoResults[i] == results2[ , 1])
+  }
+
+return(selectedProbesFromGpr[,-1])
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
